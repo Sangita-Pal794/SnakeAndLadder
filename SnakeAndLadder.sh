ca48bc46bc7fc489a32222907abef5fe9c64f553
Sangita <pal07sangita@gmail.com>
2020-08-02 20:14:35 +0530

    function to decide which type of move

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,4 +1,4 @@
-!/bin/bash -x
+#!/bin/bash -x
 
 echo "Welcome To Snake And Ladder!!"
 
@@ -7,3 +7,12 @@ StartPos=0
 
 Dice=$(( 1 + $RANDOM%6 ))
 echo $Dice
+move_type=`shuf -n 1 -e LADDER SNAKE NOPLAY`
+
+checkOption(){
+	case $move_type in
+			NOPLAY)echo $(($1+0));;
+			LADDER)echo $(($1+Dice));;
+			SNAKE) echo $(($1-Dice));;
+	esac
+}

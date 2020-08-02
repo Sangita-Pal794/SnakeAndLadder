c7adbce847d3bd273bd255523b02ccfd554591b4
Sangita <pal07sangita@gmail.com>
2020-08-02 21:09:13 +0530

    Added UC4:repearting till the player reaches 100

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -4,10 +4,16 @@ echo "Welcome To Snake And Ladder!!"
 
 WinPos=100
 StartPos=0
-
+#To store the count of no. of timed dice thrown
+dice1count=0
+#To store the movement of player position
+TempPos=0
+PlayerOnePos=$(($StartPos))
+rolleddice(){
 Dice=$(( 1 + $RANDOM%6 ))
 echo $Dice
 move_type=`shuf -n 1 -e LADDER SNAKE NOPLAY`
+}
 
 checkOption(){
 	case $move_type in
@@ -16,3 +22,12 @@ checkOption(){
 			SNAKE) echo $(($1-Dice));;
 	esac
 }
+
+while [ $PlayerOnePos -lt $WinPos ]
+do
+rolleddice
+((dice1count++))
+TempPos=$(checkOption $PlayerOnePos)
+PlayerOnePos=$(($TempPos))
+done
+echo "Player One is currently at $PlayerOnePos after throwing $dice1count times"
